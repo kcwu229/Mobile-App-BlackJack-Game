b70058a62fbdcd5d1c0edc01ebae9c3f4e849402
Wu Ka Cheong <79691025+kcwu229@users.noreply.github.com>
2024-07-25 21:23:45 +0800

    updated the ui and login function

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,13 @@ class _MyHomePageState extends State<MyHomePage> {
                           foregroundColor: Colors.white,
                           backgroundColor: Color.fromRGBO(83, 102, 94, 50),
                           minimumSize: const Size.fromHeight(50)),
-                      onPressed: () {},
+                      onPressed: () {
+                        // Navigate to the new page
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => MainPage()),
+                        );
+                      },
                       child: Text(
                         'Start Game',
                         style: TextStyle(fontSize: 25),
