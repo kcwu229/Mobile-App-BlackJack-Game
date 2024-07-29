9d453c15199601c85f289ce415bfaba727c7d861
Wu Ka Cheong <79691025+kcwu229@users.noreply.github.com>
2024-07-29 15:09:48 +0800

    cardDesign & layout

--- lib/pages/quickPage.dart
+++ lib/pages/quickPage.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blackjack/pages/card.dart';
+import 'package:flutter_blackjack/pages/cardBack.dart';
 
 // one of the function on mainPage
 class QuickGamePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.green,
       appBar: AppBar(
         title: Text('New Page'),
       ),
       body: Center(
-        child: Text('This is the new page'),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [MyCard(), MyCard()],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    child: RotatedBox(
+                      quarterTurns: 1,
+                      child: Row(
+                        children: [MyCard(), MyCard()],
+                      ),
+                    ),
+                  ),
+                  // Deck for drawing card
+                  CardBack(),
+                  Container(
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Row(
+                        children: [MyCard(), MyCard()],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [MyCard(), MyCard()],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
