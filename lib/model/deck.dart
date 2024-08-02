1fd213ea50a5e7bdcd6202bb321feb05beb10364
Wu Ka Cheong <79691025+kcwu229@users.noreply.github.com>
2024-08-02 21:11:46 +0800

    update the UI and developing the game flow

--- lib/model/deck.dart
+++ lib/model/deck.dart
@@ -1,6 +1,52 @@
-class Deck {
-  bool shuffled;
-  int remaining;
+import 'package:flutter/rendering.dart';
+import 'package:flutter_blackjack/model/card.dart';
+import 'dart:developer' as developer;
 
-  Deck(this.remaining, this.shuffled);
+enum Suits { spade, heart, club, diamond }
+
+enum Number {
+  ace,
+  two,
+  three,
+  four,
+  five,
+  six,
+  seven,
+  eight,
+  nine,
+  ten,
+  jack,
+  queen,
+  king
+}
+
+class Deck {
+  final List<MyCard> deck = [];
+
+// intial create a deck
+  Deck() {
+    for (final suit in Suits.values) {
+      for (final num in Number.values) {
+        deck.add(MyCard(suit: suit, number: num));
+      }
+    }
+  }
+
+  // shuffle
+  void _shuffle() {
+    deck.shuffle();
+  }
+
+  // drawing 2 cards in first round
+  drawTwoCard() {
+    return deck.removeRange(0, 2);
+  }
+
+  // drawing 1 card in later round
+  drawCard() {
+    if (deck.isEmpty) {
+      throw Exception('The deck is empty !');
+    }
+    return deck.removeRange(0, 1);
+  }
 }
