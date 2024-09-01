a8a1615064c777ab543d48d38df5e972cbba1590
Wu Ka Cheong <79691025+kcwu229@users.noreply.github.com>
2024-09-01 15:47:48 +0800

    part 3

--- lib/model/dialog.dart
+++ lib/model/dialog.dart
@@ -1,40 +1,170 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-showDialogWindow(context, width, height, imageFiles) {
+import 'package:flutter/material.dart';
+import 'package:flutter_blackjack/model/userData.dart';
+import 'package:flutter_blackjack/pages/mainPage.dart';
+
+Future<String?> showDialogWindow(context, width, height, imageFiles) {
+  final imageHeight = height / 10;
+  final imageWidth = width / 8;
+  int amountPerRow = 3;
+  bool preview = false;
+  String imageName = '';
+  String appliedBG = '';
+  List<String> imageList = imageFiles.sublist(amountPerRow);
+
+  Completer<String?> completer = Completer();
   showDialog(
       context: context,
       barrierDismissible: true,
       builder: (BuildContext context) {
-        return Container(
-            alignment: Alignment.center,
-            child: AlertDialog(
-              backgroundColor: Color.fromARGB(255, 247, 247, 248),
-              alignment: Alignment.center,
-              title: Text(
-                'Welcome',
-                style: TextStyle(color: Colors.black),
-              ),
-              content: SizedBox(
-                  width: width * 0.9,
-                  height: height * 0.9,
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Row(
-                          children: [
-                            for (var imageFile in imageFiles)
-                              Container(
-                                width: width / 3,
-                                height: height / 5,
-                                decoration: BoxDecoration(
-                                    image: DecorationImage(
-                                        image: AssetImage(imageFile))),
-                              )
-                          ],
-                        )
-                      ])),
-              actions: [],
-            ));
+        return FutureBuilder<Map<String, dynamic>>(
+          future: loadBG(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final userData = snapshot.data!;
+              appliedBG = userData['appliedBG'];
+              print('This is : ${appliedBG}');
+              return dialogContent(
+                context,
+                width,
+                height,
+                imageFiles,
+                preview,
+                amountPerRow,
+                imageName,
+                imageHeight,
+                imageWidth,
+              );
+            } else if (snapshot.hasError) {
+              return Text('Error loading user data: ${snapshot.error}');
+            } else {
+              return CircularProgressIndicator(); // or some other loading indicator
+            }
+          },
+        );
       });
+
+  return completer.future;
+}
+
+Widget iconButton(context, width, iconShape, function) {
+  return IconButton(
+      color: const Color.fromARGB(255, 240, 116, 8),
+      iconSize: width / 12,
+      onPressed: () => function,
+      icon: Icon(iconShape));
+}
+
+Widget closeButton(context, width, iconShape) {
+  return IconButton(
+      color: Colors.black,
+      iconSize: width / 30,
+      onPressed: () => Navigator.of(context).pop(),
+      icon: Icon(iconShape));
+}
+
+moveToLeft(context) {
+  return null;
+}
+
+moveToRight(context) {
+  return null;
+}
+
+Widget dialogContent(
+  context,
+  width,
+  height,
+  imageFiles,
+  preview,
+  int amountPerRow,
+  imageName,
+  imageHeight,
+  imageWidth,
+) {
+  return Dialog(
+      //set as full screen
+      insetPadding: EdgeInsets.zero,
+      child: StatefulBuilder(builder: (context, setState) {
+        return Container(
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage((preview == false)
+                        ? 'assets/img/background/bride_2.jpg'
+                        : imageName),
+                    fit: BoxFit.cover)),
+            child: Column(children: [
+              Expanded(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      Expanded(
+                          flex: 9,
+                          child: Container(
+                            height: height / 10,
+                          )),
+                      // close button
+                      Expanded(
+                        flex: 1,
+                        child: closeButton(context, width, Icons.close),
+                      )
+                    ],
+                  )),
+              Expanded(
+                  flex: 8,
+                  child: SizedBox(
+                    height: height / 6,
+                  )),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                iconButton(context, width, Icons.arrow_left, null),
+                for (int i = 0; i < 5; i += amountPerRow)
+                  Padding(
+                    padding: EdgeInsets.all(width / 120),
+                    child: InkWell(
+                      onTap: () {
+                        setState(() {
+                          preview = true;
+                          imageName = imageFiles[i];
+                          print(imageName);
+                        });
+                      },
+                      child: Container(
+                        width: imageWidth,
+                        height: imageHeight,
+                        decoration: BoxDecoration(
+                            image: DecorationImage(
+                                image: AssetImage(imageFiles[i]),
+                                fit: BoxFit.cover)),
+                      ),
+                    ),
+                  ),
+                iconButton(context, width, Icons.arrow_right, null),
+                MaterialButton(
+                  onPressed: () => {
+                    setState(() {
+                      saveBG(imageName);
+                    }),
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MainPageWidget()),
+                    ),
+                  },
+                  child: Container(
+                    width: width / 10,
+                    height: height / 10,
+                    color: Colors.orange,
+                    child: Text(
+                      'Apply',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: width / 38,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                  ),
+                )
+              ]),
+            ]));
+      }));
 }
--- lib/pages/mainPage.dart
+++ lib/pages/mainPage.dart
@@ -9,7 +9,6 @@ import 'package:flutter_blackjack/model/musicPlayer.dart';
 import 'package:flutter_blackjack/model/userData.dart';
 import 'package:flutter_blackjack/pages/bettingPage.dart';
 import 'package:flutter_blackjack/pages/settingPage.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 // shift to the main page
 
@@ -30,8 +29,8 @@ class _MainPageWidgetState extends State<MainPageWidget> {
     Icons.facebook,
     Icons.shopify,
   ];
-  bool changedBG = false;
   String folderPath = 'assets/img/background';
+  String BGFileName = '';
 
   @override
   void initState() {
@@ -45,12 +44,8 @@ class _MainPageWidgetState extends State<MainPageWidget> {
 
   // STOP HERE
   Future _initImages() async {
-    // >> To get paths you need these 2 lines
     final manifestContent = await rootBundle.loadString('AssetManifest.json');
-
     final Map<String, dynamic> manifestMap = json.decode(manifestContent);
-    // >> To get paths you need these 2 lines
-
     imagePaths = manifestMap.keys
         .where((String key) => key.contains('assets/img/background'))
         .toList();
@@ -58,6 +53,12 @@ class _MainPageWidgetState extends State<MainPageWidget> {
 
   void retrieveImageList() {
     _initImages();
+  }
+
+  void updateBackgroundImage(String newBGFileName) {
+    setState(() {
+      BGFileName = newBGFileName;
+    });
   }
 
   @override
@@ -73,100 +74,130 @@ class _MainPageWidgetState extends State<MainPageWidget> {
     double width = mediaQuery.size.width;
     double iconHeight = height / 12;
     double iconWidth = width / 25;
-    final prefs = SharedPreferences.getInstance();
+    //final prefs = SharedPreferences.getInstance();
 
-    return Scaffold(
-      body: PopScope(
-          canPop: false,
-          child: Container(
-            width: width,
-            height: height,
-            decoration: BoxDecoration(
-                image: DecorationImage(
-              image: AssetImage(changedBG
-                  ? "assets/img/background/mainPage.gif"
-                  : 'assets/img/background/summer.jpg'),
-              fit: BoxFit.cover,
-            )),
-            child: Column(
-              children: <Widget>[
-                Expanded(
-                  flex: 1,
-                  child: Row(
-                    children: [
-                      Transform(
-                          alignment: Alignment.topRight,
-                          transform: Matrix4.identity()
-                            ..translate(width / 19, height / 100, 0.0),
-                          child: userIcon(width / 5, height / 5)),
-                      Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            userInfo(width / 4.5, height / 20, height / 30),
-                            Container(
-                                color: const Color.fromARGB(154, 59, 59, 59),
-                                child: Row(children: [
-                                  for (var iconItem in iconList)
-                                    iconPanels(
-                                        iconHeight,
-                                        iconWidth,
-                                        musicplayer,
-                                        context,
-                                        iconItem,
-                                        BattlePage()),
-                                ])),
-                          ]),
-                      SizedBox(width: width / 10),
-                      coinField(width / 5, height / 7, 'coin', 'coin',
-                          musicplayer, context),
-                      SizedBox(width: width / 40),
-                      coinField(width / 5, height / 7, 'gachaCoin', 'gachaCoin',
-                          musicplayer, context),
-                    ],
-                  ),
-                ),
-                Expanded(
-                    flex: 2,
-                    child: Row(
-                      children: <Widget>[
-                        Expanded(flex: 5, child: Container()),
-                        mainPanels(height / 3, width, musicplayer, context,
-                            'quickGame', 'Quick Game', QuickGamePage()),
-                        Padding(padding: EdgeInsets.all(2)),
-                        mainPanels(height / 3, width, musicplayer, context,
-                            'battle', 'Battle', BattlePage()),
-                        Expanded(flex: 1, child: Container()),
-                      ],
+    return FutureBuilder<Map<String, dynamic>>(
+        future: loadBG(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final userData = snapshot.data!;
+            final BGFileName = userData['appliedBG'];
+            String currentBGFileName = BGFileName;
+
+            return Scaffold(
+              body: PopScope(
+                  canPop: false,
+                  child: Container(
+                    width: width,
+                    height: height,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                      image: AssetImage((currentBGFileName != '')
+                          ? currentBGFileName
+                          : 'assets/img/background/mainPage.gif'),
+                      fit: BoxFit.cover,
                     )),
-                Padding(padding: EdgeInsets.all(10)),
-                Expanded(
-                    flex: 1,
-                    child: Row(
+                    child: Column(
                       children: <Widget>[
-                        Expanded(flex: 1, child: Container()),
+                        Expanded(
+                          flex: 1,
+                          child: Row(
+                            children: [
+                              Transform(
+                                  alignment: Alignment.topRight,
+                                  transform: Matrix4.identity()
+                                    ..translate(width / 19, height / 100, 0.0),
+                                  child: userIcon(width / 5, height / 5)),
+                              Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    userInfo(
+                                        width / 4.5, height / 20, height / 30),
+                                    Container(
+                                        color: const Color.fromARGB(
+                                            154, 59, 59, 59),
+                                        child: Row(children: [
+                                          for (var iconItem in iconList)
+                                            iconPanels(
+                                                iconHeight,
+                                                iconWidth,
+                                                musicplayer,
+                                                context,
+                                                iconItem,
+                                                BattlePage()),
+                                        ])),
+                                  ]),
+                              SizedBox(width: width / 10),
+                              coinField(width / 5, height / 7, 'coin', 'coin',
+                                  musicplayer, context),
+                              SizedBox(width: width / 40),
+                              coinField(width / 5, height / 7, 'gachaCoin',
+                                  'gachaCoin', musicplayer, context),
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                            flex: 2,
+                            child: Row(
+                              children: <Widget>[
+                                Expanded(flex: 5, child: Container()),
+                                mainPanels(
+                                    height / 3,
+                                    width,
+                                    musicplayer,
+                                    context,
+                                    'quickGame',
+                                    'Quick Game',
+                                    QuickGamePage()),
+                                Padding(padding: EdgeInsets.all(2)),
+                                mainPanels(height / 3, width, musicplayer,
+                                    context, 'battle', 'Battle', BattlePage()),
+                                Expanded(flex: 1, child: Container()),
+                              ],
+                            )),
+                        Padding(padding: EdgeInsets.all(10)),
                         Expanded(
                             flex: 1,
-                            child: SizedBox(
-                                height: iconHeight * 2,
-                                width: iconWidth * 2,
-                                child: IconButton(
-                                  icon: Icon(
-                                    Icons.change_circle_outlined,
-                                    size: iconHeight,
-                                    color: Colors.white,
-                                  ),
-                                  onPressed: () async {
-                                    showDialogWindow(
-                                        context, width, height, imagePaths);
-                                  },
-                                ))),
-                        Expanded(flex: 8, child: Container()),
+                            child: Row(
+                              children: <Widget>[
+                                Expanded(flex: 1, child: Container()),
+                                Expanded(
+                                    flex: 1,
+                                    child: SizedBox(
+                                        height: iconHeight * 2,
+                                        width: iconWidth * 2,
+                                        child: IconButton(
+                                          icon: Icon(
+                                            Icons.change_circle_outlined,
+                                            size: iconHeight,
+                                            color: Colors.white,
+                                          ),
+                                          onPressed: () async {
+                                            var selectedImage =
+                                                await showDialogWindow(context,
+                                                    width, height, imagePaths);
+
+                                            if (selectedImage != null) {
+                                              setState(() {
+                                                currentBGFileName =
+                                                    selectedImage;
+                                              });
+                                            }
+                                          },
+                                        ))),
+                                Expanded(flex: 8, child: Container()),
+                              ],
+                            )),
                       ],
-                    )),
-              ],
-            ),
-          )),
-    );
+                    ),
+                  )),
+            );
+          } else if (snapshot.hasError) {
+            return Text('Error loading user data: ${snapshot.error}');
+          } else {
+            return CircularProgressIndicator();
+          }
+        });
   }
 }
